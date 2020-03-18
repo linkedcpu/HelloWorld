b0d5659cbeebd83cfdef65c271ced609c30869f5
linkedcpu <458875122@qq.com>
2020-03-19 01:40:22 +0800

    修改了setVR

--- skinFramework.py
+++ skinFramework.py
@@ -7,15 +7,37 @@ class SkinFramework:
         self.cellX=int(self.x/self.numX)
         self.cellY=int(self.y/self.numY)
         self.y=self.y-self.cellY
-        
+        self.numY=self.numY-1
+        self.listVR=[]
+
         
 
     def setVR(self):
+
+        for numY in range(self.numY):
+            for numX in range(self.numX):
+                tupleVR=(numX*self.cellX,numY*self.cellY,self.cellX,self.cellY)
+                if numY==3:
+                    if numX==0 or numX==self.numX-2:
+                        tupleVR=(numX*self.cellX,numY*self.cellY,int(self.cellX*1.5),self.cellY)
+                    elif numX==1 or numX==self.numX-1:
+                        continue
+                    else:
+                        tupleVR=(int((numX-0.5)*self.cellX),numY*self.cellY,self.cellX,self.cellY)
+               
+                print(str(tupleVR)[1:-1])
+                self.listVR.append(str(tupleVR)[1:-1])
+                
+
         pass
 
 
     def test(self):
         pass
-        print(self.cellY)  
+        # print(self.listVR)
+        print(len(self.listVR))
+        
 
-SkinFramework().test()     
+sf=SkinFramework()
+sf.setVR()
+sf.test()   
